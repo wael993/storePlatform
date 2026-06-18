69c42ecfd1c5cf6a0852d0437200073051ad6666
wael zobani <wael.zobani@realcore.de>
2026-06-18 16:50:51 +0200

    get ready for hosting backend

--- services/api-store-platform/src/server.ts
+++ services/api-store-platform/src/server.ts
@@ -24,10 +24,17 @@ app.use(cookieParser())
 
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 		credentials: true,
 	}),
 )
+
+// app.use(
+// 	cors({
+// 		origin: 'http://localhost:3000',
+// 		credentials: true,
+// 	}),
+// )
 app.use(errorHandler)
 
 connectDB()
